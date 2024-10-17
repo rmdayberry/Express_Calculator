b041ace6a3337067660153aa70298b9776c9875c
rmdayberry <dayberryrm@gmail.com>
2024-10-17 13:10:19 -0400

    Build helper functions
    Built helper functions for mean, medium, and mode calculations

---
+++ .history/helpers_20241017121133.js
@@ -0,0 +1,26 @@
+function calculateMean(nums) {
+  let sum = nums.reduce((acc, curr) => acc + curr, 0);
+  return sum / nums.length;
+}
+
+function calculateMedian(nums) {
+  nums.sort((a, b) => a - b);
+  const mid = Math.floor(nums.length / 2);
+
+  return nums.length % 2 === 0 ? (nums[mid - 1] + nums[mid]) / 2 : nums[mid];
+}
+
+function calculateMode(nums) {
+  const frequency = {};
+  let maxFreq = 0;
+  let mode;
+
+  nums.forEach((num) => {
+    frequency[num] = (frequency[num] || 0) + 1;
+    if (frequency[num] > maxFreq) {
+      maxFreq = frequency[num];
+      mode = num;
+    }
+  });
+  return mode;
+}
